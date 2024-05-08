0f79b1ea7bd21e0b82304385fa68eb89ab7b76a2
raichandanisagar <raichandanisagar@gmail.com>
2024-05-08 13:02:07 -0400

    Updating audit analysis with resume scores vs. selection graph; shap pending

--- src/modelapi.py
+++ src/modelapi.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import pandas as pd
+from math import ceil
 
 
 def _build_dataframe(responses):
@@ -10,14 +11,25 @@ def _build_dataframe(responses):
     return resp_df
 
 
+def _split_rows(num_rows, max_sz):
+    start = 0
+    splits = []
+    while start < num_rows:
+        end = start + min(num_rows - start, max_sz)
+        splits.append((start, end))
+        start = end
+    return splits
+
+
 def fetch_response(url, data, max_sz, headers={'content-type': 'application/json'}, verbose=1):
     resp_store = []
-    for i in range(data.shape[0] // max_sz):
-        body = json.dumps(data.iloc[i * max_sz:(i + 1) * max_sz].to_dict('records'))
+    idx_splits = _split_rows(data.shape[0], max_sz)
+    for i, (start, end) in enumerate(idx_splits):
+        body = json.dumps(data.iloc[start:end].to_dict('records'))
         resp = requests.post(url=url, data=body, headers=headers)
         resp_store.append(resp)
         if verbose:
-            print(f"Iteration {i + 1} of {data.shape[0] // max_sz} response state: {resp.ok}")
+            print(f"Iteration {i + 1} of {len(idx_splits)} response state: {resp.ok}")
         if not resp.ok:
             print(resp.reason)
             break
